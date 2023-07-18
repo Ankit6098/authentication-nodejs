c4596f80d51af7cd8664f0d3b908849bf20d882e
Ankit <ankitvis609@gmail.com>
2023-07-18 17:39:30 +0530

    comments and line updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,78 +12,75 @@ module.exports.welcome = function(req, res) {
 
 // Function to sign in user using session 
 module.exports.createSession = async function(req, res) {
-    if (req.user) {
-      req.flash('success', 'Logged In');
-        console.log("logged in successfully");
-    } else {
-      req.flash('error', 'Invalid Username/Password');
-        console.log("invalid username/password");
-    }
+  if (req.user) {
+    req.flash('success', 'Logged In');
+      console.log("logged in successfully");
+  } else {
+    req.flash('error', 'Invalid Username/Password');
+      console.log("invalid username/password");
+  }
 
-    return res.redirect('/user/welcome');
+  return res.redirect('/user/welcome');
 }
 
 // Function to sign out and destroy session
 module.exports.destroySession = function(req, res) {
-    req.logout(function(error) {
-      req.session.destroy();
-        if (error) {
-            req.flash('error', 'Something went wrong!');
-            return;
-        }
-    });
-    req.flash('success', 'Logged Out');
-    return res.redirect('/');
-    
+  req.logout(function(error) {
+    req.session.destroy();
+      if (error) {
+          req.flash('error', 'Something went wrong!');
+          return;
+      }
+  });
+  req.flash('success', 'Logged Out');
+  return res.redirect('/');
 }
 
 module.exports.signup = function(req, res) {
-    if(req.isAuthenticated()) {
-        return res.redirect('/user/welcome');
-    }
+  if(req.isAuthenticated()) {
+      return res.redirect('/user/welcome');
+  }
 
-    return res.render('user_sign_up', {
-        title: 'Nodejs Authentication'
-    });
+  return res.render('user_sign_up', {
+      title: 'Nodejs Authentication'
+  });
 }
 
-// const User = require('../models/user');
-
 module.exports.create = async function(req, res) {
-    if (req.body.password != req.body.confirmPassword) {
-        console.log('Passwords do not match!');
-        return res.redirect('back');
-    }
+  if (req.body.password != req.body.confirmPassword) {
+      console.log('Passwords do not match!');
+      return res.redirect('back');
+  }
 
-    // find user if exists
-    const user = await User.findOne({email: req.body.email});
+  // find user if exists
+  const user = await User.findOne({email: req.body.email});
 
-    if (!user) {
-        const plaintextPassword = req.body.password;
-        const saltRounds = 10;
-      
-        // Generate a hash of the password
-        bcrypt.hash(plaintextPassword, saltRounds, async (err, hash) => {
-          if (err) {
-            console.error(err);
-            return res.status(500).send('Error creating user');
-          }
-      
-          try {
-            const newUser = await User.create({
-              ...req.body,
-              password: hash // Store the hashed password in the database
-            });
-            console.log('New user created!');
-            return res.redirect('/');
-          } catch (err) {
-            console.error(err);
-            return res.status(500).send('Error creating user');
-          }
-        });
-      } else {
-        console.log('User already exists!');
-        req.flash('info', 'User already exists!');
-        return res.redirect('back');
-      }
+  if (!user) {
+      const plaintextPassword = req.body.password;
+      const saltRounds = 10;
+    
+      // Generate a hash of the password
+      bcrypt.hash(plaintextPassword, saltRounds, async (err, hash) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Error creating user');
+        }
+    
+        try {
+          const newUser = await User.create({
+            ...req.body,
+            password: hash // Store the hashed password in the database
+          });
+          console.log('New user created!');
+          return res.redirect('/');
+        } catch (err) {
+          console.error(err);
+          return res.status(500).send('Error creating user');
+        }
+      });
+    } else {
+      console.log('User already exists!');
+      req.flash('info', 'User already exists!');
+      return res.redirect('back');
+  }
 }
